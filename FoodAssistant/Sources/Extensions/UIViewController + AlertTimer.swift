44016ec2a19c76444fc0f13a560c13f1adbe5ad1
rubis-vladimir <84345727+rubis-vladimir@users.noreply.github.com>
2023-01-05 14:16:19 +0300

    Планирую делать таймер и тесты

---
+++ FoodAssistant/Sources/Extensions/UIViewController + AlertTimer.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController + AlertTimer.swift
+//  FoodAssistant
+//
+//  Created by Владимир Рубис on 05.01.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showTimer() {
+        
+    }
+}
