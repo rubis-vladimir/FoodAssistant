69d8ed84b220fc961f32b022fd643c66c81361e9
rubis-vladimir <84345727+rubis-vladimir@users.noreply.github.com>
2023-01-06 12:42:07 +0300

    Рефакторинг кода. Коммит для предварительной сдачи
    Начал написание тестов

---
+++ FoodAssistant/Sources/🚀 Apps/ℹ️ DetailInfo/Views/Table/Instructions/InstructionCell.swift
@@ -0,0 +1,115 @@
+//
+//  InstructionCell.swift
+//  FoodAssistant
+//
+//  Created by Владимир Рубис on 28.11.2022.
+//
+
+import UIKit
+
+/// #Протокол передачи UI-ивента нажатия на кнопку таймера
+protocol TimerTapable: AnyObject {
+    
+    /// Нажата кнопка таймера
+    /// - Parameter step: Шаг в инструкции
+    func didTapTimerButton(step: Int)
+}
+
+/// #Ячейка с инструкцией по приготовлению
+final class InstructionCell: TVBaseCell {
+    
+    weak var delegate: TimerTapable?
+    
+    private var step: Int?
+    
+    private lazy var stepNumberLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.main
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var cookingInstructionsLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.main
+        label.textAlignment = .justified
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var timerButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(Icons.alarm.image, for: .normal)
+        
+        button.tintColor = Palette.darkColor.color
+        return button
+    }()
+    
+    private lazy var container: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 16
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var stack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 10
+        return stack
+    }()
+    
+    override func setupCell() {
+//        contentView.backgroundColor = .clear
+        
+        timerButton.addTarget(self,
+                              action: #selector(didTapTimerButton),
+                              for: .touchUpInside)
+        
+        stack.addArrangedSubview(stepNumberLabel)
+        
+        stack.addArrangedSubview(timerButton)
+        [stack, cookingInstructionsLabel].forEach {
+            container.addArrangedSubview($0)
+        }
+        
+        contentView.addSubview(container)
+        setupConstraints()
+    }
+    
+    func configure(with step: InstructionStepProtocol) {
+        let stepText = "Шаг".localize()
+        stepNumberLabel.text = "\(stepText) \(step.number)"
+        cookingInstructionsLabel.text = step.step
+        self.step = step.number
+        
+        timerButton.isHidden = !isTime(from: step.step)
+        
+        contentView.layoutSubviews()
+    }
+    
+    private func setupConstraints() {
+
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            container.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            container.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            container.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    /// Проверяет есть ли время в тексте
+    /// - Parameter text: текст
+    private func isTime(from text: String) -> Bool {
+        return text.contains("minut".localize()) || text.contains("hour".localize()) ? true : false
+    }
+    
+    @objc private func didTapTimerButton() {
+        guard let step = step else { return }
+        delegate?.didTapTimerButton(step: step)
+    }
+}
+
---
+++ FoodAssistant/Sources/🚀 Apps/👤 UserProfile/UserProfilePresenter.swift
@@ -0,0 +1,262 @@
+//
+//  UserProfilePresenter.swift
+//  FoodAssistant
+//
+//  Created by Владимир Рубис on 30.10.2022.
+//
+
+import Foundation
+
+/// #Навигация в модуле UserProfile
+enum UserProfileTarget {
+    /// Детальная информация
+    case detailInfo(_ recipe: RecipeProtocol)
+}
+
+/// #Протокол управления слоем навигации модуля UserProfile
+protocol UserProfileRouting {
+    /// Перейти к следующему экрану
+    ///  - Parameter to: вариант перехода
+    func route(to: UserProfileTarget)
+}
+
+/// #Протокол управления View-слоем модуля UserProfile
+protocol UserProfileViewable: ErrorShowable,
+                              AnyObject {
+    /// Обновить `Collection View`
+    /// - Parameter orderSection: подгружаемая секция
+    func updateCV(orderSection: [UPSectionType])
+    /// Обновить `Nav Bar`
+    /// - Parameter index: выбранный индекс сегмента
+    func updateNavBar(index: Int)
+    /// Показать алерт добавления ингредиента
+    /// - Parameter completion: захватывает модель ингредиента/ ошибку
+    func showAlert(completion: @escaping (Result<IngredientViewModel, DataFetcherError>) -> Void)
+    /// Перезагрузить элементы
+    /// - Parameter items: перезагружаемые элементы
+    func reload(items: [IndexPath])
+}
+
+/// #Протокол управления бизнес логикой модуля UserProfile
+protocol UserProfileBusinessLogic: RecipeReceived,
+                                   ImageBusinessLogic,
+                                   IngredientFetchable,
+                                   InBasketAdded,
+                                   RecipeRemovable {
+    /// Получить ингредиенты
+    /// - Parameters:
+    ///  - text: текст в поисковом баре
+    ///  - completion: захватывает вьюмодели рецептов
+    func fetchFavoriteRecipe(text: String?,
+                             completion: @escaping ([RecipeViewModel]) -> Void)
+    
+    /// Найти ингредиент (для определения идентификатора)
+    /// - Parameters:
+    ///  - ingredient: вью-модель ингредиента
+    ///  - flag: флаг использования
+    func find(ingredient: IngredientViewModel,
+              completion: @escaping (Result<IngredientViewModel, DataFetcherError>) -> Void)
+    
+    /// Изменить флаг использования ингредиента
+    /// - Parameters:
+    ///  - id: идентификатор ингредиента
+    ///  - flag: флаг использования
+    func changeToUse(id: Int, flag: Bool)
+    
+    /// Сохранить ингредиент
+    /// - Parameter ingredient: ингредиент
+    func save(ingredient: IngredientProtocol)
+    
+    /// Удалить ингредиент
+    /// - Parameter id: идентификатор ингредиента
+    func deleteIngredient(id: Int)
+}
+
+
+// MARK: - Presenter
+/// #Слой презентации модуля UserProfile
+final class UserProfilePresenter {
+    /// Текущий сегмент
+    private var currentSegmentIndex = 1
+    /// Вью-модели рецептов
+    private var recipes: [RecipeViewModel] = []
+    /// Вью-модели ингредиентов
+    private var ingredients: [IngredientViewModel] = []
+    
+    weak var view: UserProfileViewable?
+    private let interactor: UserProfileBusinessLogic
+    private let router: UserProfileRouting
+    
+    init(interactor: UserProfileBusinessLogic,
+         router: UserProfileRouting) {
+        self.interactor = interactor
+        self.router = router
+    }
+    
+    
+    func getStart() {
+        interactor.fetchIngredients { ingredients in
+            self.ingredients = ingredients
+        }
+        
+        view?.updateNavBar(index: currentSegmentIndex)
+        view?.updateCV(orderSection: [.fridge(ingredients)])
+    }
+    
+    /// Конфигурирует и показывает восстанавливаемую ошибку
+    /// - Parameters:
+    ///  - error: ошибка
+    ///  - action: действи при восстановлении
+    private func showRecoveryError(from error: DataFetcherError,
+                                   action: @escaping () -> ()) {
+        var actions: [RecoveryOptions] = [.cancel]
+        
+        switch error {
+        case .invalidNumber, .notDataProvided:
+            let tryAgainAction = RecoveryOptions.tryAgain(action: action)
+            actions.append(tryAgainAction)
+        default: break
+        }
+        
+        view?.show(rError: RecoverableError(error: error,
+                                            recoveryOptions: actions))
+    }
+    
+    private func updateSelectedSegment(index: Int) {
+        switch index {
+        /// вкладка профиля
+        case 0:
+            view?.updateCV(orderSection: [.profile])
+            
+        /// вкладка холодильника
+        case 1:
+            interactor.fetchIngredients { ingredients in
+                self.ingredients = ingredients
+            }
+            view?.updateCV(orderSection: [.fridge(ingredients)])
+        
+        /// вкладка избранных рецептов
+        default:
+            interactor.fetchFavoriteRecipe(text: nil) { [weak self] viewModels in
+                self?.recipes = viewModels
+            }
+            view?.updateCV(orderSection: [.favorite(recipes)])
+        }
+        view?.updateNavBar(index: index)
+        
+        currentSegmentIndex = index
+    }
+}
+
+
+// MARK: - UserProfilePresentation
+extension UserProfilePresenter: UserProfilePresentation {
+    
+    func didTapAddIngredientButton() {
+        view?.showAlert(completion: { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let ingredient):
+                self.interactor.find(ingredient: ingredient) { result in
+                    
+                    switch result {
+                    case .success(let newViewModel):
+                        self.ingredients.append(newViewModel)
+                        self.view?.updateCV(orderSection: [.fridge(self.ingredients)])
+                        
+                    case .failure(let error):
+                        /// Пока не обрабатывается
+                        print(error.localizedDescription)
+                    }
+                }
+                
+            case .failure(let error):
+                switch error {
+                case .invalidNumber, .notDataProvided:
+                    let action = { self.didTapAddIngredientButton() }
+                    self.showRecoveryError(from: error,
+                                           action: action)
+                default:
+                    print(error.localizedDescription)
+                }
+            }
+        })
+    }
+    
+    func textEntered(_ text: String) {
+        interactor.fetchFavoriteRecipe(text: text) {[weak self] models in
+            self?.recipes = models
+        }
+    }
+    
+    func checkFlag(id: Int) -> Bool {
+        guard let index = ingredients.firstIndex(where: { $0.id == id }) else { return false }
+        return ingredients[index].toUse
+    }
+    
+    // ViewAppearable
+    func viewAppeared() {
+        updateSelectedSegment(index: currentSegmentIndex)
+    }
+    
+    // SegmentedViewDelegate
+    func didSelectSegment(index: Int) {
+        
+        guard index != currentSegmentIndex else { return }
+        updateSelectedSegment(index: index)
+    }
+    
+    // ImagePresentation
+    func fetchImage(_ imageName: String,
+                    type: TypeOfImage,
+                    completion: @escaping (Data) -> Void) {
+        interactor.fetchImage(imageName, type: type) { result in
+            switch result {
+            case .success(let data):
+                completion(data)
+            case .failure(let error):
+                /// Пока не обрабатывается
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // RecipeRemovable
+    func didTapDeleteButton(id: Int) {
+        
+        switch currentSegmentIndex {
+        case 1:
+            interactor.deleteIngredient(id: id)
+            
+            guard let index = ingredients.firstIndex(where: {$0.id == id} ) else { break }
+            ingredients.remove(at: index)
+            
+        case 2:
+            interactor.removeRecipe(id: id)
+            
+            guard let index = recipes.firstIndex(where: {$0.id == id} ) else { break }
+            recipes.remove(at: index)
+        default: break
+        }
+        
+        updateSelectedSegment(index: currentSegmentIndex)
+    }
+    
+    // InBasketAdded
+    func didTapAddInBasketButton(id: Int) {
+        interactor.addToBasket(id: id)
+    }
+    
+    // SelectedCellDelegate
+    func didSelectItem(id: Int) {
+        interactor.getRecipe(id: id) { [weak self] recipe in
+            self?.router.route(to: .detailInfo(recipe))
+        }
+    }
+    
+    // CheckChangable
+    func didTapCheckButton(id: Int, flag: Bool) {
+        interactor.changeToUse(id: id, flag: flag)
+    }
+}
